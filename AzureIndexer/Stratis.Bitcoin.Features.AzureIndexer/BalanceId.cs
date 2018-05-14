18bbe78441936563c3ebcb575f5487f6d8161681
SomeGuy FromAfrica <someguy.fromafrica@gmail.com>
2018-05-14 15:25:46 +1000

    Add comments to BalanceId class

--- AzureIndexer/Stratis.Bitcoin.Features.AzureIndexer/BalanceId.cs
+++ AzureIndexer/Stratis.Bitcoin.Features.AzureIndexer/BalanceId.cs
@@ -10,92 +10,136 @@ namespace Stratis.Bitcoin.Features.AzureIndexer
         Wallet,
         Address
     }
+
     public class BalanceId
     {
+        /// <summary>Balance id prefix to use when using a wallet id as the id.</summary>
         const string WalletPrefix = "w$";
+
+        /// <summary>Balance id prefix to use when using a script hash as the id.</summary>
         const string HashPrefix = "h$";
 
+        /// <summary>Maximum script size that can be used as-is in the id - otherwise the script hash is used.</summary>
         internal const int MaxScriptSize = 79;
+
+        /// <summary>The cached partition key.</summary>
+        string partitionKey;
+
+        /// <summary>The balance id.</summary>
+        string balanceId;
+
+        /// <summary>
+        /// Constructor for constructing a balance id from a wallet id.
+        /// </summary>
+        /// <param name="walletId">The wallet id to build the balance id from.</param>
         public BalanceId(string walletId)
         {
-            _Internal = WalletPrefix + FastEncoder.Instance.EncodeData(Encoding.UTF8.GetBytes(walletId));
+            balanceId = WalletPrefix + FastEncoder.Instance.EncodeData(Encoding.UTF8.GetBytes(walletId));
         }
+
+        /// <summary>
+        /// Constructor for constructing a balance id from a script.
+        /// </summary>
+        /// <param name="scriptPubKey">The script to build the balance id from.</param>
+        /// <remarks>The MaxScriptSize determines the maximum script size that can be used as-is in the id - otherwise the script hash is used.</remarks>
         public BalanceId(Script scriptPubKey)
         {
             var pubKey = scriptPubKey.ToBytes(true);
+
             if (pubKey.Length > MaxScriptSize)
-                _Internal = HashPrefix + FastEncoder.Instance.EncodeData(scriptPubKey.Hash.ToBytes(true));
+                balanceId = HashPrefix + FastEncoder.Instance.EncodeData(scriptPubKey.Hash.ToBytes(true));
             else
-                _Internal = FastEncoder.Instance.EncodeData(scriptPubKey.ToBytes(true));
+                balanceId = FastEncoder.Instance.EncodeData(scriptPubKey.ToBytes(true));
         }
+
+        /// <summary>
+        /// Constructor for constructing a balance id from a destination.
+        /// </summary>
+        /// <param name="destination">The destination to build the balance id from.</param>
+        /// <remarks>The MaxScriptSize determines the maximum script size that can be used as-is in the id - otherwise the script hash is used.</remarks>
         public BalanceId(IDestination destination)
             : this(destination.ScriptPubKey)
         {
         }
 
+        /// <summary>
+        /// Private parameter-less constructor.
+        /// </summary>
         private BalanceId()
         {
-
         }
 
+        /// <summary>
+        /// Recovers the wallet id from the internal id if it was constucted from a wallet id.
+        /// </summary>
+        /// <returns>The wallet id.</returns>
+        /// <exception cref="System.InvalidOperationException">Thrown if the internal id was not constructed from a wallet id.</exception>
         public string GetWalletId()
         {
-            if (!_Internal.StartsWith(WalletPrefix))
+            if (Type != BalanceType.Wallet)
                 throw new InvalidOperationException("This balance id does not represent a wallet");
-            return Encoding.UTF8.GetString(FastEncoder.Instance.DecodeData(_Internal.Substring(WalletPrefix.Length)));
+
+            return Encoding.UTF8.GetString(FastEncoder.Instance.DecodeData(balanceId.Substring(WalletPrefix.Length)));
         }
 
+        /// <summary>
+        /// Determines if this object was constructed from a wallet id or a script.
+        /// </summary>
         public BalanceType Type
         {
             get
             {
-                return _Internal.StartsWith(WalletPrefix) ? BalanceType.Wallet : BalanceType.Address;
+                return balanceId.StartsWith(WalletPrefix) ? BalanceType.Wallet : BalanceType.Address;
             }
         }
 
+        /// <summary>
+        /// Returns the partition key.
+        /// </summary>
         public string PartitionKey
         {
             get
             {
-                if (_PartitionKey == null)
-                {
-                    _PartitionKey = Helper.GetPartitionKey(10, Crc32.Compute(_Internal));
-                }
-                return _PartitionKey;
+                // Calculate the partition key if not calculated yet.
+                if (partitionKey == null)
+                    partitionKey = Helper.GetPartitionKey(10, Crc32.Compute(balanceId));
+
+                return partitionKey;
             }
         }
 
+        /// <summary>
+        /// Extracts the script that was used to construct this object, if any, and only if the script size does not exceed MaxScriptSize.
+        /// </summary>
+        /// <returns>The script that was used to construct this object - otherwise returns null.</returns>
         public Script ExtractScript()
         {
-            if (!ContainsScript)
-                return null;
-            return Script.FromBytesUnsafe(FastEncoder.Instance.DecodeData(_Internal));
+            return ContainsScript?Script.FromBytesUnsafe(FastEncoder.Instance.DecodeData(balanceId)):null;
         }
 
-        public bool ContainsScript
-        {
-            get
-            {
-                return _Internal.Length >= 2 && _Internal[1] != '$';
-            }
-        }
+        /// <summary>
+        /// Determines if a script, with a size not exceeding MaxScriptSize, was used to construct this object.
+        /// </summary>
+        public bool ContainsScript => balanceId.Length >= 2 && balanceId[1] != '$';
 
-
-        string _PartitionKey;
-        string _Internal;
+        /// <summary>
+        /// Returns the balance id.
+        /// </summary>
         public override string ToString()
         {
-            return _Internal;
+            return balanceId;
         }
 
+        /// <summary>
+        /// Sets the balance id directly.
+        /// </summary>
+        /// <param name="balanceId">The balance id to set.</param>
         public static BalanceId Parse(string balanceId)
         {
             return new BalanceId()
             {
-                _Internal = balanceId
+                balanceId = balanceId
             };
         }
-
-        
     }
 }
