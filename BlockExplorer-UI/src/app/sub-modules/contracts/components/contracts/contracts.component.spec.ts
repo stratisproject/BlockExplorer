29c0007ea7f4e8c5d5514f7272098c013f28c584
Paul Herbert <paul.herbert@stratisplatform.com>
2018-11-09 11:49:54 +0000

    BlockExplorerUI latest: Contacts component pagination work.

--- BlockExplorer-UI/src/app/sub-modules/contracts/components/contracts/contracts.component.spec.ts
+++ BlockExplorer-UI/src/app/sub-modules/contracts/components/contracts/contracts.component.spec.ts
@@ -1,25 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContractsComponent } from './contracts.component';
+import { SearchTextService } from 'src/app/services/search-text.service';
+import { FakeApiService } from 'src/app/services/api.service';
 
 describe('ContractsComponent', () => {
-  let component: ContractsComponent;
-  let fixture: ComponentFixture<ContractsComponent>;
+    let component: ContractsComponent;
+    let apiService: FakeApiService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ContractsComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(() => {
+        apiService = new FakeApiService();
+        component = new ContractsComponent(new SearchTextService(), apiService);
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ContractsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('currentPageDisplay set to 1 when firstPageClicked', () => {
+        component.firstPageClicked();
+        expect(component.currentPageDisplay).toEqual('1');
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('currentPageDisplay set to 1 when firstPageClicked clicked in succession', () => {
+        component.firstPageClicked();
+        component.firstPageClicked();
+        expect(component.currentPageDisplay).toEqual('1');
+    });
+
+    it('currentPageDisplay set to 2 when nextPageClicked clicked', () => {
+        component.nextPageClicked();
+        expect(component.currentPageDisplay).toEqual('2');
+    });
+
+    it('currentPageDisplay set to 1 when nextPageClicked followed by lastPageClicked', () => {
+        component.nextPageClicked();
+        component.previousPageClicked();
+        expect(component.currentPageDisplay).toEqual('1');
+    });
+
+    it('currentPageDisplay set to lastPage when lastPageClicked', () => {
+        component.lastPageClicked();
+        expect(component.currentPageDisplay).toEqual(component.pageCount.toString());
+    });
+
+    it('currentPageDisplay set to lastPage when lastPageClicked clicked in succession', () => {
+        component.lastPageClicked();
+        component.lastPageClicked();
+        expect(component.currentPageDisplay).toEqual(component.pageCount.toString());
+    });
 });
--- BlockExplorer-UI/src/app/sub-modules/contracts/components/contracts/contracts.component.ts
+++ BlockExplorer-UI/src/app/sub-modules/contracts/components/contracts/contracts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { SearchTextService } from 'src/app/services/search-text.service';
 import { ApiServiceBase } from 'src/app/services/api.service';
@@ -8,19 +8,54 @@ import { ApiServiceBase } from 'src/app/services/api.service';
     templateUrl: './contracts.component.html',
     styleUrls: ['./contracts.component.scss']
 })
-export class ContractsComponent implements OnInit {
-    private pageCount = 0;
+export class ContractsComponent  {
     private readonly maxContractsPerPage = 8;
+    private currentPage = -1;
 
     constructor(readonly searchTextService: SearchTextService, readonly apiService: ApiServiceBase) {
         searchTextService.searchTextStream.subscribe(x => console.log(x));
         apiService.getContractCountAsync().subscribe(x => this.processContractCount(x));
+
+        this.setPage(0);
     }
 
-    ngOnInit() {
+    pageCount = 0;
+
+    get currentPageDisplay(): string { return (this.currentPage+1).toString(); }
+
+    firstPageClicked() {
+        this.setPage(0);
+    }
+
+    previousPageClicked() {
+        let page = this.currentPage;
+        if (page - 1 >= 0) {
+            this.setPage(--page);
+        }
+    }
+
+    nextPageClicked() {
+        let page = this.currentPage;
+        if (page + 1 < this.pageCount) {
+            this.setPage(++page);
+        }
+    }
+
+    lastPageClicked() {
+        this.setPage(this.pageCount-1);
+    }
+
+    private setPage(page: number) {
+        console.assert(page >= 0 && page < this.pageCount, `page out of range : ${page}`);
+        if (page === this.currentPage) {
+            return;
+        }
+        this.currentPage = page;
+        console.log("Page : " + (this.currentPage+1).toString());
     }
 
     private processContractCount(contractCount: number) {
+        this.pageCount = 0;
         if (contractCount > 0) {
             if (contractCount <= this.maxContractsPerPage) {
                 this.pageCount = 1;
