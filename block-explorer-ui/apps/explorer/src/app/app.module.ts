708cdf309578e005da2ceae7a8032e8253789698
Igor Goldobin <igor.goldobin@gmail.com>
2019-02-01 00:15:50 +1000

    Fixed code highlighting

--- block-explorer-ui/apps/explorer/src/app/app.module.ts
+++ block-explorer-ui/apps/explorer/src/app/app.module.ts
@@ -29,6 +29,8 @@ import { StateGlobalStateModule } from '@blockexplorer/state/global-state';
 import { ENVIRONMENT, SharedUtilsModule } from '@blockexplorer/shared/utils';
 import { UiSmartContractsModule } from '@blockexplorer/ui/smart-contracts';
 
+import 'prismjs/components/prism-csharp';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
