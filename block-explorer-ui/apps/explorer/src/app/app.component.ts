e51cfffde9bb10a7e70b0ab7c4eb9407c43fddf2
fenix2222 <igor.goldobin@gmail.com>
2019-08-21 23:46:45 +1000

    fix for script address

--- block-explorer-ui/apps/explorer/src/app/app.component.ts
+++ block-explorer-ui/apps/explorer/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 import { GlobalFacade } from '@blockexplorer/state/global-state';
 import { Observable, ReplaySubject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'blockexplorer-root',
@@ -20,42 +20,43 @@ export class AppComponent implements OnDestroy {
   constructor(private globalFacade: GlobalFacade, private router: Router) {
     this.found$ = this.globalFacade.loaded$;
     this.found$.pipe(takeUntil(this.destroyed$))
-        .subscribe(data => {
-          this.found = data;
-        });
+      .subscribe(data => {
+        this.found = data;
+      });
     this.identifiedEntity$ = this.globalFacade.identifiedEntity$;
     this.identifiedEntity$.pipe(takeUntil(this.destroyed$))
-        .subscribe(entity => {
-          let type = 'UNKNOWN';
-          if (entity === null) {
+      .subscribe(entity => {
+        let type = 'UNKNOWN';
+        if (entity === null) {
+          this.router.navigate(['search', 'not-found']);
+          return;
+        }
+
+        if (entity === undefined) return;
+
+        if (!!entity.type) type = entity.type;
+        if (!!entity.transaction) type = 'TRANSACTION';
+        if (!!entity.additionalInformation) type = 'BLOCK';
+
+        switch (type) {
+          case 'PUBKEY_ADDRESS':
+          case 'SCRIPT_ADDRESS':
+            this.router.navigate(['addresses', this.text]);
+            break;
+          case 'TRANSACTION':
+            this.router.navigate(['transactions', this.text]);
+            break;
+          case 'BLOCK':
+            this.router.navigate(['blocks', this.text]);
+            break;
+          default:
             this.router.navigate(['search', 'not-found']);
-            return;
-          }
-
-          if (entity === undefined) return;
-
-          if (!!entity.type) type = entity.type;
-          if (!!entity.transaction) type = 'TRANSACTION';
-          if (!!entity.additionalInformation) type = 'BLOCK';
-
-          switch (type) {
-            case 'PUBKEY_ADDRESS':
-              this.router.navigate(['addresses', this.text]);
-              break;
-            case 'TRANSACTION':
-              this.router.navigate(['transactions', this.text]);
-              break;
-            case 'BLOCK':
-              this.router.navigate(['blocks', this.text]);
-              break;
-            default:
-              this.router.navigate(['search', 'not-found']);
-              break;
-          }
-        });
+            break;
+        }
+      });
   }
 
-  find(text: string){
+  find(text: string) {
     this.text = text;
     this.globalFacade.identify(text);
   }
