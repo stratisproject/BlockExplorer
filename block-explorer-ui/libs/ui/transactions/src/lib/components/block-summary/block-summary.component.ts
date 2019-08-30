5137725b4744291b475557b15d24b310f69dc204
fenix2222 <igor.goldobin@gmail.com>
2019-08-30 16:33:30 +1000

    Updated reward code

--- block-explorer-ui/libs/ui/transactions/src/lib/components/block-summary/block-summary.component.ts
+++ block-explorer-ui/libs/ui/transactions/src/lib/components/block-summary/block-summary.component.ts
@@ -2,51 +2,61 @@ import { Component, Input, OnInit } from '@angular/core';
 import { BlockResponseModel } from '@blockexplorer/shared/models';
 
 @Component({
-  selector: 'blockexplorer-block-summary',
-  templateUrl: './block-summary.component.html',
-  styleUrls: ['./block-summary.component.css']
+    selector: 'blockexplorer-block-summary',
+    templateUrl: './block-summary.component.html',
+    styleUrls: ['./block-summary.component.css']
 })
 export class BlockSummaryComponent implements OnInit {
 
-  @Input() block: BlockResponseModel = null;
+    @Input() block: BlockResponseModel = null;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  public get size() {
-    return this.block.extendedInformation.size || 0;
-  }
+    public get size() {
+        return this.block.extendedInformation.size || 0;
+    }
 
-  public get transactionCount() {
-    return this.block.extendedInformation.transactionCount || 0;
-  }
+    public get transactionCount() {
+        return this.block.extendedInformation.transactionCount || 0;
+    }
 
-  public get blockReward() {
-    return !!this.block.extendedInformation && !!this.block.extendedInformation.blockReward
-      ? this.block.extendedInformation.blockReward.satoshi || 0
-      : 0;
-  }
+    public get blockReward() {
+        return !!this.block.extendedInformation && !!this.block.extendedInformation.blockReward
+            ? this.block.extendedInformation.blockReward.satoshi || 0
+            : 0;
+    }
 
-  public get confirmations() {
-    return this.block.additionalInformation.confirmations || 0;
-  }
+    public get blockSubsidy() {
+        return !!this.block.extendedInformation && !!this.block.extendedInformation.blockSubsidy
+            ? this.block.extendedInformation.blockSubsidy.satoshi || 0
+            : 0;
+    }
 
-  public get height() {
-    return this.block.additionalInformation.height || 0;
-  }
+    public get totalBlockReward() {
+        return this.blockReward + this.blockSubsidy;
+    }
 
-  public get hash() {
-    return this.block.additionalInformation.blockId || '';
-  }
+    public get confirmations() {
+        return this.block.additionalInformation.confirmations || 0;
+    }
 
-  public get time() {
-    if (!this.block || !this.block.block.header.time) return 'Unknown';
-    const date = new Date(1000 * this.block.block.header.time);
+    public get height() {
+        return this.block.additionalInformation.height || 0;
+    }
 
-    return date.toLocaleString();
-    // TODO: decide which format we want to show date in.
-    // return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-  }
+    public get hash() {
+        return this.block.additionalInformation.blockId || '';
+    }
+
+    public get time() {
+        if (!this.block || !this.block.block.header.time) return 'Unknown';
+        const date = new Date(1000 * this.block.block.header.time);
+
+        return date.toLocaleString();
+        // TODO: decide which format we want to show date in.
+        // return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+    }
 }
