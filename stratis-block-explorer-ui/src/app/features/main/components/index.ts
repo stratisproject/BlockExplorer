90495cff96dea70df0c3856c1075b8ecf1dffa35
Mithril Man <red.angel@libero.it>
2019-11-23 11:24:37 +0100

    moved main from component to container

--- stratis-block-explorer-ui/src/app/features/main/components/index.ts
+++ stratis-block-explorer-ui/src/app/features/main/components/index.ts
@@ -1,4 +1,3 @@
 export { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 export { MenuBarComponent } from './menu-bar/menu-bar.component';
-export { MainComponent } from './main/main.component';
 export { SidenavListComponent } from './sidenav-list/sidenav-list.component';
--- stratis-block-explorer-ui/src/app/features/main/main.module.ts
+++ stratis-block-explorer-ui/src/app/features/main/main.module.ts
@@ -3,36 +3,32 @@ import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import * as fromComponents from './components';
+import * as fromContainers from './containers';
 import * as fromStore from './store';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
 import { SidenavListComponent } from './components/sidenav-list/sidenav-list.component';
 
-const exportedComponents: Type<any>[] = [
-   fromComponents.MenuBarComponent,
-   fromComponents.BreadcrumbComponent,
-   fromComponents.MainComponent,
-   fromComponents.SidenavListComponent
-]
-
 
 @NgModule({
-   declarations: [
-      ...exportedComponents,
-      SidenavListComponent
-   ],
-   imports: [
-      CommonModule,
-      FormsModule,
-      RouterModule,
-      SharedModule,
-      StoreModule.forFeature(fromStore.mainFeatureKey, fromStore.reducer),
-      EffectsModule.forFeature([fromStore.MainEffects])
-   ],
-   exports: [
-      fromComponents.MainComponent,
-      //...exportedComponents
-   ]
+    declarations: [
+        fromComponents.MenuBarComponent,
+        fromComponents.BreadcrumbComponent,
+        fromComponents.SidenavListComponent,
+        fromContainers.MainComponent,
+        SidenavListComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule,
+        SharedModule,
+        StoreModule.forFeature(fromStore.mainFeatureKey, fromStore.reducer),
+        EffectsModule.forFeature([fromStore.MainEffects])
+    ],
+    exports: [
+        fromContainers.MainComponent,
+    ]
 })
 export class MainModule { }
