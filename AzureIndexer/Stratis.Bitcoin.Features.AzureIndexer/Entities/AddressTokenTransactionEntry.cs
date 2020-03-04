ee9bb26067fca72a76beb7de285f014a0a093a8d
Rowan de Haas <rowandh@users.noreply.github.com>
2020-03-05 10:53:30 +1100

    Add AddressTokenTransaction entity

---
+++ AzureIndexer/Stratis.Bitcoin.Features.AzureIndexer/Entities/AddressTokenTransactionEntry.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Stratis.Bitcoin.Features.AzureIndexer
+{
+    public class AddressTokenTransactionEntry : TableEntity
+    {
+        public string Address
+        {
+            get => this.PartitionKey;
+            set => this.PartitionKey = value;
+        }
+
+        public string AddressTo { get; set; }
+
+        public string Amount { get; set; } // Use a string because of big integers
+
+        public long BlockHeight { get; set; } // Long is the maximum supported integer value for table storage
+
+        public static string CreateRowKey(int blockHeight, string txId)
+        {
+            return $"{blockHeight}:{txId}";
+        }
+    }
+}
