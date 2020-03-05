6594a17a68335215597f0fc532452efbbaac6027
Rowan de Haas <rowandh@users.noreply.github.com>
2020-03-05 14:51:27 +1100

    Begin implementation of IndexTokensTask

--- AzureIndexer/Stratis.Bitcoin.Features.AzureIndexer/IndexTasks/IndexTokensTask.cs
+++ AzureIndexer/Stratis.Bitcoin.Features.AzureIndexer/IndexTasks/IndexTokensTask.cs
@@ -1,4 +1,9 @@
-﻿namespace Stratis.Bitcoin.Features.AzureIndexer.IndexTasks
+﻿using System.Linq;
+using Stratis.Bitcoin.Features.AzureIndexer.Tokens;
+using Stratis.SmartContracts.Core.Receipts;
+using Stratis.SmartContracts.Core.State;
+
+namespace Stratis.Bitcoin.Features.AzureIndexer.IndexTasks
 {
     using System.Collections.Generic;
     using Microsoft.Extensions.Logging;
@@ -8,11 +13,15 @@
 
     public class IndexTokensTask : IndexTableEntitiesTaskBase<AddressTokenTransactionEntry>
     {
+        private readonly IReceiptRepository receiptRepository;
+        private readonly IStateRepositoryRoot state;
         private readonly ILogger logger;
 
-        public IndexTokensTask(IndexerConfiguration configuration, ILoggerFactory loggerFactory) 
+        public IndexTokensTask(IndexerConfiguration configuration, ILoggerFactory loggerFactory, IReceiptRepository receiptRepository, IStateRepositoryRoot state) 
             : base(configuration, loggerFactory)
         {
+            this.receiptRepository = receiptRepository;
+            this.state = state;
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
         }
 
@@ -21,9 +30,59 @@
             return this.Configuration.GetAddressTokenTransactionTable();
         }
 
-        protected override void ProcessBlock(BlockInfo block, BulkImport<AddressTokenTransactionEntry> bulkImport, Network network, BulkImport<SmartContactEntry.Entity> SmartContractBulk = null)
+        protected override void ProcessBlock(BlockInfo block, BulkImport<AddressTokenTransactionEntry> bulk, Network network, BulkImport<SmartContactEntry.Entity> smartContractBulk = null)
         {
-            throw new System.NotImplementedException();
+            var blockHeight = block.Height;
+
+            foreach (Transaction tx in block.Block.Transactions)
+            {
+                var txHash = tx.GetHash();
+
+                Receipt receipt = this.receiptRepository.Retrieve(txHash);
+
+                // Ignore non-SC transactions or failed transactions
+                if (receipt == null || receipt.Success == false)
+                {
+                    continue;
+                }
+
+                // TODO DI
+                var tokenEntityMapper = new TokenDetailProvider(this.receiptRepository, this.state);
+
+                var tokenDetail = tokenEntityMapper.Get(blockHeight, txHash);
+
+                // Hang on, there's actually three addresses involved here:
+                // The sender of the tokens
+                // The recipient of the tokens
+                // The token contract address
+
+                var fromEntity = new AddressTokenTransactionEntry
+                {
+                    PartitionKey = tokenDetail.From,
+                    RowKey = AddressTokenTransactionEntry.CreateRowKey(blockHeight, txHash),
+                    BlockHeight = blockHeight,
+                    AddressTo = tokenDetail.To,
+                    Amount = tokenDetail.Amount,
+                    TokenAddress = tokenDetail.TokenAddress,
+                    TokenSymbol = tokenDetail.TokenSymbol
+                };
+
+                bulk.Add(fromEntity.PartitionKey, fromEntity);
+
+                // Add this entity so we can also perform the reverse lookup on the recipient's address
+                var toEntity = new AddressTokenTransactionEntry
+                {
+                    PartitionKey = tokenDetail.To,
+                    RowKey = AddressTokenTransactionEntry.CreateRowKey(blockHeight, txHash),
+                    BlockHeight = blockHeight,
+                    AddressTo = tokenDetail.From,
+                    Amount = $"-{tokenDetail.Amount}", // TODO does this make sense?
+                    TokenAddress = tokenDetail.TokenAddress,
+                    TokenSymbol = tokenDetail.TokenSymbol
+                };
+
+                bulk.Add(toEntity.PartitionKey, toEntity);
+            }
         }
 
         protected override ITableEntity ToTableEntity(AddressTokenTransactionEntry item)
---
+++ AzureIndexer/Stratis.Bitcoin.Features.AzureIndexer/Tokens/TokenDetailProvider.cs
@@ -0,0 +1,55 @@
+﻿using NBitcoin;
+using Stratis.SmartContracts.Core.Receipts;
+using Stratis.SmartContracts.Core.State;
+
+namespace Stratis.Bitcoin.Features.AzureIndexer.Tokens
+{
+    public class TokenTransferDetails
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string TokenAddress { get; set; }
+
+        public string TokenSymbol { get; set; }
+
+        public string Amount { get; set; }
+    }
+
+    /// <summary>
+    /// Returns information about a token transfer.
+    /// </summary>
+    public class TokenDetailProvider
+    {
+        private readonly IReceiptRepository receiptRepository;
+        private readonly IStateRepositoryRoot state;
+        private readonly TokenIdentifier tokenIdentifier;
+
+        public TokenDetailProvider(IReceiptRepository receiptRepository, IStateRepositoryRoot state)
+        {
+            this.receiptRepository = receiptRepository;
+            this.state = state;
+            this.tokenIdentifier = new TokenIdentifier(state); // TODO inject
+        }
+
+        public TokenTransferDetails Get(int blockHeight, uint256 txHash)
+        {
+            Receipt receipt = this.receiptRepository.Retrieve(txHash);
+
+            // Ignore non-SC transactions or failed transactions
+            if (receipt == null || receipt.Success == false)
+            {
+                return null;
+            }
+
+            // Ignore non token-transfers
+            if (!this.tokenIdentifier.IsToken(receipt))
+            {
+                return null;
+            }
+
+            return new TokenTransferDetails();
+        }
+    }
+}
