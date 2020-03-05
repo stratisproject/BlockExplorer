fa2f300edc2aee349e4b78b09cb806d36f26d260
Rowan de Haas <rowandh@users.noreply.github.com>
2020-03-05 14:50:59 +1100

    Add additional fields to entity

--- AzureIndexer/Stratis.Bitcoin.Features.AzureIndexer/Entities/AddressTokenTransactionEntry.cs
+++ AzureIndexer/Stratis.Bitcoin.Features.AzureIndexer/Entities/AddressTokenTransactionEntry.cs
@@ -5,6 +5,9 @@ namespace Stratis.Bitcoin.Features.AzureIndexer
 {
     public class AddressTokenTransactionEntry : TableEntity
     {
+        /// <summary>
+        /// The sender address.
+        /// </summary>
         public string Address
         {
             get => this.PartitionKey;
@@ -13,9 +16,13 @@ namespace Stratis.Bitcoin.Features.AzureIndexer
 
         public string AddressTo { get; set; }
 
+        public string TokenAddress { get; set; }
+
+        public string TokenSymbol { get; set; }
+
         public string Amount { get; set; } // Use a string because of big integers
 
-        public long BlockHeight { get; set; } // Long is the maximum supported integer value for table storage
+        public long BlockHeight { get; set; } // Long is the maximum supported integer value for table storage. Duplicated here as well in the rowKey for better querying.
 
         public static string CreateRowKey(int blockHeight, uint256 txId)
         {
