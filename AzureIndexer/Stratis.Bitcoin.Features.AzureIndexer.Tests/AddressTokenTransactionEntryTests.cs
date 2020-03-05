a4e41d13f03cec7e2fb09906751c0f99e6ea5e51
Rowan de Haas <rowandh@users.noreply.github.com>
2020-03-05 11:10:58 +1100

    Add test

---
+++ AzureIndexer/Stratis.Bitcoin.Features.AzureIndexer.Tests/AddressTokenTransactionEntryTests.cs
@@ -0,0 +1,19 @@
+﻿using NBitcoin;
+using Xunit;
+
+namespace Stratis.Bitcoin.Features.AzureIndexer.Tests
+{
+    public class AddressTokenTransactionEntryTests
+    {
+        [Fact]
+        public void CreateRowKey_Success()
+        {
+            var txid = uint256.One;
+            var blockHeight = 543421;
+
+            var key = AddressTokenTransactionEntry.CreateRowKey(blockHeight, txid);
+
+            Assert.Equal("543421:0000000000000000000000000000000000000000000000000000000000000001", key);
+        }
+    }
+}
