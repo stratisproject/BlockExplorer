6a7c7eeaef94bc35a585a22364662f4f0f1124a7
Rowan de Haas <rowandh@users.noreply.github.com>
2020-03-10 17:48:33 +1100

    Fix selector path

--- block-explorer-ui/libs/state/tokens-state/src/index.ts
+++ block-explorer-ui/libs/state/tokens-state/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/+state/tokens.facade';
 export * from './lib/+state/tokens.reducer';
-export * from './lib/+state/transactions.selectors';
+export * from './lib/+state/tokens.selectors';
 export * from './lib/state-tokens-state.module';
