f5ecdb9ec26b58ff70895c21a936881228e52548
Igor Goldobin <igor.goldobin@bhp.com>
2021-03-01 16:54:29 +1000

    Removed duplicate registration

--- AzureIndexer/Stratis.IndexerD/Program.cs
+++ AzureIndexer/Stratis.IndexerD/Program.cs
@@ -68,7 +68,6 @@ namespace Stratis.IndexerD
                         .UseAzureIndexerOnSideChain()
                         .UseApi()
                         .UseMempool()
-                        .UseAzureIndexer()
                         .Build();
                 }
                 else
